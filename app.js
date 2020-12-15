1974454bafa1eb0d27aaaed1e1c4671fd757a325
abdallahadel <abdallah.m.adel@gmail.com>
2020-12-15 20:11:15 +0200

    Session updates

--- app.js
+++ app.js
@@ -9,27 +9,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/scripts', express.static(__dirname + '/scripts/'));
-
+var session = require('express-session');
 /*var dumbo = {table:[],books:[]};
 dumbo.books.push({name:"lord of flies",direct:'/flies'})
 dumbo.books.push({name:"the grapes of wrath",direct:'/grapes'})
 dumbo.books.push({name:"leaves of grass",direct:'/leaves'})
 dumbo.books.push({name:"the sun and her flowers",direct:'/sun'})
-dumbo.books.push({name:"dune",direct:'/dune'})
+dumbo.books.push({name:"dune",direct:'/dune'}) 
 dumbo.books.push({name:"to kill a mockingbird",direct:'/mockingbird'})
 var s = JSON.stringify(dumbo);
 fs.writeFileSync("users.json",s);*/
-
+app.use(session({ secret: 'keyboard cat',resave:false,saveUninitialized:false, cookie: { maxAge: 6000}}));
 var books = {Books:[]};
 var booksStringfy = JSON.stringify(books);
 fs.writeFileSync("books.json",booksStringfy);
 
 app.get('/login',function(req,res){
     res.render('login' , {error: ""});
-});
-
+}); 
 app.get('/',function(req,res){
-    res.redirect('login');
+    if (req.session.user){
+    req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.redirect('home');
+
+        }
+        else{
+            res.redirect('/login')
+        }
 });
 
 app.get('/registration',function(req,res){
@@ -37,38 +44,124 @@ app.get('/registration',function(req,res){
 });
 
 app.get('/home',function(req,res){
+    if (req.session.user){
+    req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
     res.render('home');
+    }
+    else{
+        res.redirect('/login')
+    }
 });
 
 app.get('/fiction',function(req,res){
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
     res.render('fiction');
+    }
+    else{
+        res.redirect('/login')
+    }
 })
 app.get('/novel',function(req,res){
-    res.render('novel');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('novel');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
 })
 app.get('/poetry',function(req,res){
-    res.render('poetry');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('poetry');
+        }
+        else{
+            res.redirect('/login')
+        }
 })
 app.get('/flies',function(req,res){
-    res.render('flies');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('flies');
+        }
+        else{
+            res.redirect('/login')
+        }
 })
 app.get('/grapes',function(req,res){
-    res.render('grapes');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('grapes');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
 })
 app.get('/leaves',function(req,res){
-    res.render('leaves');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('leaves');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
+    
 })
 app.get('/sun',function(req,res){
-    res.render('sun');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('sun');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
 })
 app.get('/mockingbird',function(req,res){
-    res.render('mockingbird');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+        req.session.cookie.maxAge = 5000
+        res.render('sun');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
 })
 app.get('/dune',function(req,res){
-    res.render('dune');
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('dune');
+        }
+        else{
+            res.redirect('/login')
+        }
+    
+    
 })
 app.get('/readlist',function(req,res){
-    res.render('readlist' , {booklist :books.Books});
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+    req.session.cookie.maxAge = 5000
+        res.render('readlist' , {booklist :books.Books});
+        }
+        else{
+            res.redirect('/login')
+        }
+    
 })
 
 app.post('/register',function(req,res){
@@ -91,7 +184,6 @@ app.post('/register',function(req,res){
         } else { res.render('registration' , {error: "" , error2: "The username or password cannot be empty."}); }
     }   
 });
-
 app.post('/login',function(req,res){
     var z = fs.readFileSync("users.json");
     dumbo = JSON.parse(z);
@@ -100,6 +192,7 @@ app.post('/login',function(req,res){
     var found = dumbo.table.some(e => e.user == nameLow && e.password == req.body.password);
 
     if (found){
+        req.session.user = nameLow;
         res.redirect('home');
     } else{
         res.render('login' , {error: "The username or password are incorrect."});
@@ -131,11 +224,25 @@ function pushBook(title, link) {
     var booksStringfy = JSON.stringify(books);
     fs.writeFileSync("books.json",booksStringfy);
 }
-
+app.get('/searchresults',function(req,res){
+    if (req.session.user){
+        req.session.cookie.expires = new Date(Date.now() + 5000)
+        req.session.cookie.maxAge = 5000
+        if(searchresults.books.length != 0){
+            res.render('searchresults' , {contents: searchresults.books , zero:""})
+        }
+        else{
+            res.render('searchresults' , {contents: 0, zero:"No results"})
+        }
+        }
+        else{
+            res.redirect('/login')
+        }
+})
+var searchresults ={books:[]}
 app.post('/search', function(req,res){
     var read = fs.readFileSync("users.json");
     var dumbo = JSON.parse(read);
-    var searchresults ={books:[]}
     var zzz = req.body.Search;
     var namelow = zzz.toLowerCase();
     for(i = 0; i<dumbo.books.length; i++){
@@ -143,12 +250,7 @@ app.post('/search', function(req,res){
             searchresults.books.push(dumbo.books[i]);
         }
     }
-    if(searchresults.books.length != 0){
-        res.render('searchresults' , {contents: searchresults.books , zero:""})
-    }
-    else{
-        res.render('searchresults' , {contents: 0, zero:"No results"})
-    }
+    res.redirect('/searchresults')
 })
 
 app.listen(3003);
